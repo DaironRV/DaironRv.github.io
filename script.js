41630f0d6e7986af0a51d9977b342ada913b7618
DaironRV <131011899+DaironRV@users.noreply.github.com>
2023-09-20 10:06:34 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,135 @@
+document.getElementById('img-1').addEventListener('click', function(){
+    
+    Swal.fire({
+
+        imageUrl: 'images/1.png',
+        imageWidth: 650,
+        imageHeight: 650,
+        imageAlt: 'Custom image',
+        timer: 5000,
+        timerProgressBar: true,
+        showConfirmButton: false, 
+        showCloseButton: true,
+        background: '#D36424',
+    })
+    
+});
+
+document.getElementById('img-2').addEventListener('click', function(){
+    
+    Swal.fire({
+
+        imageUrl: 'images/2.png',
+        imageWidth: 650,
+        imageHeight: 650,
+        imageAlt: 'Custom image',
+        timer: 5000,
+        timerProgressBar: true,
+        showConfirmButton: false, 
+        showCloseButton: true,
+        background: '#D36424',
+    })
+    
+});
+
+document.getElementById('img-3').addEventListener('click', function(){
+    
+    Swal.fire({
+
+        imageUrl: 'images/3.png',
+        imageWidth: 650,
+        imageHeight: 650,
+        imageAlt: 'Custom image',
+        timer: 5000,
+        timerProgressBar: true,
+        showConfirmButton: false, 
+        showCloseButton: true,
+        background: '#D36424',
+    })
+    
+});
+
+document.getElementById('img-4').addEventListener('click', function(){
+    
+    Swal.fire({
+
+        imageUrl: 'images/4.png',
+        imageWidth: 650,
+        imageHeight: 650,
+        imageAlt: 'Custom image',
+        timer: 5000,
+        timerProgressBar: true,
+        showConfirmButton: false, 
+        showCloseButton: true,
+        background: '#D36424',
+    })
+    
+});
+
+document.getElementById('img-5').addEventListener('click', function(){
+    
+    Swal.fire({
+
+        imageUrl: 'images/5.png',
+        imageWidth: 650,
+        imageHeight: 650,
+        imageAlt: 'Custom image',
+        timer: 5000,
+        timerProgressBar: true,
+        showConfirmButton: false, 
+        showCloseButton: true,
+        background: '#D36424',
+    })
+    
+});
+
+document.getElementById('img-6').addEventListener('click', function(){
+    
+    Swal.fire({
+
+        imageUrl: 'images/6.png',
+        imageWidth: 650,
+        imageHeight: 650,
+        imageAlt: 'Custom image',
+        timer: 5000,
+        timerProgressBar: true,
+        showConfirmButton: false, 
+        showCloseButton: true,
+        background: '#D36424',
+    })
+    
+});
+
+document.getElementById('img-7').addEventListener('click', function(){
+    
+    Swal.fire({
+
+        imageUrl: 'images/7.png',
+        imageWidth: 650,
+        imageHeight: 650,
+        imageAlt: 'Custom image',
+        timer: 5000,
+        timerProgressBar: true,
+        showConfirmButton: false, 
+        showCloseButton: true,
+        background: '#D36424',
+    })
+    
+});
+
+document.getElementById('img-8').addEventListener('click', function(){
+    
+    Swal.fire({
+
+        imageUrl: 'images/8.png',
+        imageWidth: 650,
+        imageHeight: 650,
+        imageAlt: 'Custom image',
+        timer: 5000,
+        timerProgressBar: true,
+        showConfirmButton: false, 
+        showCloseButton: true,
+        background: '#D36424',
+    })
+    
+});
